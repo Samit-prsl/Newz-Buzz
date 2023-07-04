ede1e300250f501f0fe9c59acf0d13b70e176cac
Ayush Singh <iamayush449@gmail.com>
2023-07-04 11:06:56 -0700

    Change UI added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,19 +5,19 @@ export default function Navbar() {
     <div>
        <ul className="nav justify-content-center my-2">
   <li className="nav-item">
-        <Link className="nav-link active" aria-current="page" to="/" style={{color:'wheat',fontFamily:'Libre Baskerville, serif',fontSize:'20px'}}>Home</Link>
+        <Link className="nav-link active" aria-current="page" to="/" style={{color:'wheat',fontfamily: 'Poppins',fontSize:'20px'}}>Home</Link>
   </li>
   <li className="nav-item">
-    <Link className="nav-link active" to="/National" style={{color:'wheat',fontFamily:'Libre Baskerville, serif',fontSize:'20px'}}>National</Link>
+    <Link className="nav-link active" to="/National" style={{color:'wheat',fontfamily: 'Poppins',fontSize:'20px'}}>National</Link>
   </li>
   <li className="nav-item">
-    <Link className="nav-link active" to="/International" style={{color:'wheat',fontFamily:'Libre Baskerville, serif',fontSize:'20px'}}>International</Link>
+    <Link className="nav-link active" to="/International" style={{color:'wheat',fontfamily: 'Poppins',fontSize:'20px'}}>International</Link>
   </li>
   <li className="nav-item">
-    <Link className="nav-link active" to="/Sports" style={{color:'wheat',fontFamily:'Libre Baskerville, serif',fontSize:'20px'}}>Sports</Link>
+    <Link className="nav-link active" to="/Sports" style={{color:'wheat',fontfamily: 'Poppins',fontSize:'20px'}}>Sports</Link>
   </li>
   <li className="nav-item">
-    <Link className="nav-link active" to="/Entertainment" style={{color:'wheat',fontFamily:'Libre Baskerville, serif',fontSize:'20px'}}>Entertainment</Link>
+    <Link className="nav-link active" to="/Entertainment" style={{color:'wheat',fontfamily: 'Poppins',fontSize:'20px'}}>Entertainment</Link>
   </li>
  
 </ul> 
